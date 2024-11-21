1db88a8c0c748119413dec59f6c1e9b53b514d45
Leonardo Sigolo <leonardo.sigolo1037@gmail.com>
2024-11-21 11:37:08 -0300

    comtestair

--- src/app/model/service/products.service.spec.ts
+++ src/app/model/service/products.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProductsService } from './products.service';
+import { productsService } from './products.service';
 
 describe('ProductsService', () => {
-  let service: ProductsService;
+  let service: productsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductsService);
+    service = TestBed.inject(productsService);
   });
 
   it('should be created', () => {
